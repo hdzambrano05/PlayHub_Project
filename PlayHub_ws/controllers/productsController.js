3927c197ef90ef867567b4a85a699bd7a5f13cb8
Harold Zambrano <hdzambrano.2350@unicesmag.edu.co>
2024-12-23 09:24:36 -0500

    PlayHub HD V.1.1.1

--- PlayHub_ws/controllers/productsController.js
+++ PlayHub_ws/controllers/productsController.js
@@ -1,9 +1,45 @@
 const products = require('../models').products_model;
 module.exports = {
+
     list(req, res) {
         return products
             .findAll({})
             .then((products) => res.status(200).send(products))
             .catch((error) => { res.status(400).send(error); });
     },
+
+    getById(req, res) {
+
+        console.log(req.params.id);
+        return products
+            .findByPk(req.params.id)
+            .then((products) => {
+                console.log(products);
+                if (!products) {
+                    return res.status(404).send({
+                        message: 'products Not Found',
+                    });
+                }
+                return res.status(200).send(products);
+            })
+            .catch((error) =>
+                res.status(400).send(error));
+    },
+
+    add(req, res) {
+        return products
+            .create({
+                name: req.body.name,
+                description: req.body.description,
+                price: req.body.price,
+                stock: req.body.stock,
+                image_url: req.body.image_url,
+                category_id: req.body.category_id
+
+            })
+            .then((products) => res.status(201).send(products))
+            .catch((error) => res.status(400).send(error));
+    },
+
+
 }
--- PlayHub_ws/routes/products.js
+++ PlayHub_ws/routes/products.js
@@ -4,5 +4,8 @@ var router = express.Router();
 const productsController = require('../controllers').productsController;
 
 router.get('/', productsController.list);
+router.get('/:id', productsController.getById);
+router.post('/', productsController.add);
+
 
 module.exports = router;
